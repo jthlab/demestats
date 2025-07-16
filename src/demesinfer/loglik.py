a8c4196c6c97b00cf42028ea131857e0aaee3886
Jonathan Terhorst <terhorst@gmail.com>
2025-07-16 10:47:40 -0400

    fix loglik ... again

--- src/demesinfer/loglik.py
+++ src/demesinfer/loglik.py
@@ -16,65 +16,58 @@ def loglik(eta: CoalRate, r: ScalarLike, data: Float[Array, "intervals 2"]) -> S
     Args:
         eta: Coalescent rate at time t.
         r: float, the recombination rate.
-        data: the data to compute the likelihood for. The first column is the TMRCA, and the second column is the span.
+        data: the data to compute the likelihood for. The first column is the TMRCA, and
+              the second column is the span.
 
     Notes:
         - Successive spans that have the same TMRCA should be merged into one span:
           <tmrca, span1> + <tmrca, span1> = <tmrca, span + span>.
         - Missing data/padding indicated by span<=0.
     """
-
-    def p0(t0, span, t1):
-        return 0.0
-
-    def p1(t0, span, t1):
-        def f(t, y, _):
-            c = eta(t)
-            A1 = jnp.array([[-r, r, 0.0], [c, -2 * c, c], [0.0, 0.0, 0.0]])
-            A2 = jnp.array([[0.0, 0.0, 0.0], [0.0, -c, c], [0.0, 0.0, 0.0]])
-            A = jnp.where(t < t0, A1, A2)
-            return A.T @ y
-
-        y0 = jnp.array([1.0, 0.0, 0.0])
-        solver = dfx.Tsit5()
-        term = dfx.ODETerm(f)
-        jumps = jnp.append(eta.jumps, t0)
-        ssc = dfx.PIDController(rtol=1e-6, atol=1e-6, jump_ts=jumps)
-        if t1 is None:
-            # we only know that the span extended at least this far
-            sol = dfx.diffeqsolve(
-                term, solver, 0.0, t0, dt0=t0 / 100.0, y0=y0, stepsize_controller=ssc
-            )
-            return xlogy(span, sol.ys[0, 0])
-
-        m = jnp.minimum(t0, t1)
-        M = jnp.maximum(t0, t1)
-
-        check = f(0.0, y0, None)
-
-        sol = dfx.diffeqsolve(
-            term,
-            solver,
-            0.0,
-            M,
-            dt0=t0 / 100.0,
-            y0=y0,
-            stepsize_controller=ssc,
-            saveat=dfx.SaveAt(t1=True, ts=[m]),
-        )
-        (p_nr_m, p_float_m, p_coal_m), (p_nr_M, p_float_M, p_coal_M) = sol.ys
-        return jnp.where(
-            t0 < t1,
-            xlogy(span - 1, p_nr_m) + jnp.log(p_coal_M),
-            xlogy(span - 1, p_nr_M) + jnp.log(p_coal_m),
-        )
-
-    def p(t0, span, t1):
-        return jax.lax.cond(span <= 0, p0, p1, t0, span, t1)
-
     times, spans = data.T
-    t0 = times[:-1]
-    t1 = times[1:]
-    ret = jax.vmap(p)(t0, spans[:-1], t1).sum()
-    ret += p(times[-1], spans[-1], None)
-    return ret
+    i = times.argsort()
+    sorted_times = times[i]
+
+    def f(t, y, _):
+        c = eta(t)
+        A = jnp.array([[-r, r, 0.0], [c, -2 * c, c], [0.0, 0.0, 0.0]])
+        return A.T @ y
+
+    y0 = jnp.array([1.0, 0.0, 0.0])
+    solver = dfx.Tsit5()
+    term = dfx.ODETerm(f)
+    ssc = dfx.PIDController(rtol=1e-6, atol=1e-6, jump_ts=eta.jumps)
+    T = times.max()
+    sol = dfx.diffeqsolve(
+        term,
+        solver,
+        0.0,
+        T,
+        dt0=0.001,
+        y0=y0,
+        stepsize_controller=ssc,
+        saveat=dfx.SaveAt(ts=sorted_times),
+    )
+
+    # invert the sorting so that cscs matches times
+    i_inv = i.argsort()
+    cscs = sol.ys[i_inv]
+
+    @vmap
+    def p(t0, csc0, t1, csc1, span):
+        p_nr_t0, p_float_t0, p_coal_t0 = csc0
+        p_nr_t1, p_float_t1, p_coal_t1 = csc1
+        # no recomb for first span - 1 positions
+        r1 = xlogy(span - 1, p_nr_t0)
+        # coalescence at t1
+        r2 = jnp.log(eta(t1))
+        # back-coalescence process up to t1, depends to t0 >< t1
+        r3 = jnp.where(
+            t0 < t1, jnp.log(p_float_t0) - eta.R(t0, t1), jnp.log(p_float_t1)
+        )
+        return r1 + r2 + r3
+
+    ll = p(times[:-1], cscs[:-1], times[1:], cscs[1:], spans[:-1]).sum()
+    # for the last position, we only know span was at least as long
+    ll += xlogy(spans[-1], cscs[-1, 0])
+    return ll
--- tests/test_loglik.py
+++ tests/test_loglik.py
@@ -1,4 +1,8 @@
+from functools import partial
+
+import jax
 import jax.numpy as jnp
+import msprime as msp
 from pytest import fixture
 
 import demesinfer.coal_rate
@@ -25,3 +29,35 @@ def test_loglik(eta):
     # Check if the result is a valid number (not NaN or Inf)
     assert not jnp.isnan(result)
     assert not jnp.isinf(result)
+
+
+def test_sample():
+    sim = msp.simulate(
+        2,
+        mutation_rate=1e-8,
+        recombination_rate=1e-8,
+        Ne=10000,
+        length=1e7,
+        random_seed=42,
+    )
+
+    a = []
+    for tree in sim.trees():
+        t = tree.get_tmrca(0, 1)
+        if a and t == a[-1][0]:
+            a[-1][1] += tree.span
+        else:
+            a.append([tree.get_tmrca(0, 1), tree.span])
+    data = jnp.array(a)
+
+    @jax.jit
+    @partial(jax.vmap, in_axes=(0, None))
+    def f(N, data):
+        eta = demesinfer.coal_rate.PiecewiseConstant(
+            c=jnp.array([1 / 2 / N]), t=jnp.array([0.0])
+        )
+        r = 1e-8
+        return demesinfer.loglik.loglik(eta, r, data)
+
+    x = jnp.linspace(5_000, 50_000, 10)
+    result = f(x, data)

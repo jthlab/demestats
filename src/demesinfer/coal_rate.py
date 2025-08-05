fix nans in gradients

src/demesinfer/coal_rate.py
@@ -40,10 +40,15 @@ class PiecewiseConstant(CoalRate):
 
     @property
     def _ppoly(self) -> interpax.PPoly:
-        return interpax.PPoly(self.c[None], jnp.append(self.t, jnp.inf), check=False)
+        return interpax.PPoly(
+            self.c[None],
+            jnp.append(self.t, self.t[-1] + 1),
+            check=False,
+            extrapolate=True,
+        )
 
     def __call__(self, t: ScalarLike) -> Scalar:
-        return self._ppoly(t)
+        return self._ppoly(t, extrapolate=True)
 
     def R(self, a: ScalarLike, b: ScalarLike) -> Scalar:
-        return self._ppoly.integrate(a, b)
+        return self._ppoly.integrate(a, b, extrapolate=True)

src/demesinfer/event_tree.py
@@ -17,6 +17,7 @@ from loguru import logger
 from scipy.cluster.hierarchy import DisjointSet
 
 import demesinfer.events
+from demesinfer.rescale import rescale_demo
 
 from .path import Path, get_path, set_path
 from .util import unique_strs
@@ -255,7 +256,9 @@ class EventTree:
         ret["times"] = [v.pop() if len(v) == 1 else frozenset(v) for v in ret["times"]]
         return sum(map(list, ret.values()), [])
 
-    def bind(self, params: dict[Set[Path] | Path, ScalarLike]) -> dict:
+    def bind(
+        self, params: dict[Set[Path] | Path, ScalarLike], rescale: bool = False
+    ) -> dict:
         # bind the parameters to the event tree
         if not params.keys() <= set(self.variables):
             raise ValueError(
@@ -267,6 +270,9 @@ class EventTree:
                 k = frozenset([k])
             for p in k:
                 set_path(ret, p, v)
+        if rescale:
+            # rescale the demo parameters
+            ret = rescale_demo(ret, self.scaling_factor)
         return ret
 
     def _init_leaves(self):

src/demesinfer/iicr/__init__.py
@@ -55,22 +55,23 @@ class IICRCurve:
             )
         )
         demo = self.bind(params)
+        tr = t / self.et.scaling_factor
         state = _call(
-            jnp.atleast_1d(t),
+            jnp.atleast_1d(tr),
             self.et,
             self.k,
             demo,
             num_samples,
             self._aux,
         )
-        ret = dict(c=state.c, log_s=state.log_s)
+        ret = dict(c=state.c / self.et.scaling_factor, log_s=state.log_s)
         return jax.tree.map(lambda a: a.reshape(t.shape), ret)
 
     def bind(self, params: dict[Path | frozenset[Path], ScalarLike]) -> dict:
         """
         Bind the parameters to the event tree's demo.
         """
-        return self.et.bind(params)
+        return self.et.bind(params, rescale=True)
 
 
 @eqx.filter_jit

src/demesinfer/pexp.py
@@ -39,11 +39,18 @@ class PExp(NamedTuple):
         # eta(t[i]) = a[i] exp(-b[i] dt[i]) = 1 / 2 / self.N0 =>
         return -jnp.log(1 / 2 / self.N0 / self.a) / jnp.diff(self.t)
 
-    def __call__(self, u: ScalarLike, _no_searchsorted=False) -> Scalar:
+    def __call__(self, u: ScalarLike) -> Scalar:
         r"Evaluate eta(u)."
         # log eta(t) = log(N1[i]) + [(t[i+1] - u)/(t[i+1]-t[i])] log(N0[i] / N1[i]) ^for t_i <= t < t_{i+1}
         # = log(N1[i]) + [1 - (u - t[i])/(t[i+1]-t[i])] log(N0[i] / N1[i]) ^for t_i <= t < t_{i+1}
         # = log(N0[i]) - (u - t[i])/(t[i+1]-t[i])] log(N0[i] / N1[i]) for t_i <= t < t_{i+1}
+
+        # the last entry of t might be +inf, like if the deme has a start_time of +inf
+        # in this case we must assume that N0=N1
+        t = jnp.where(
+            jnp.isinf(self.t[-1]), jnp.append(self.t[:-1], 2 * self.t[-2]), self.t
+        )
+
         log_N0 = jnp.log(self.N0)
         log_N1 = jnp.log(self.N1)
         dt = jnp.diff(self.t)
@@ -53,7 +60,7 @@ class PExp(NamedTuple):
                 log_N0,
             ]
         )
-        log_eta = PPoly(c=c, x=self.t, check=False)
+        log_eta = PPoly(c=c, x=self.t, check=False, extrapolate=True)
         ret = jnp.exp(log_eta(u))
         return eqx.error_if(ret, jnp.isnan(ret), "NaN in eta")
 
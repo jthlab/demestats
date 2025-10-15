809ce97a0d905631a3847b5ee0cb75a8071fd4c6
Jonathan Terhorst <terhorst@gmail.com>
2025-10-14 21:13:11 -0400

    improve nan handling in interp

--- src/demesinfer/iicr/interp.py
+++ src/demesinfer/iicr/interp.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import diffrax as dfx
 import equinox as eqx
 import jax
@@ -25,20 +27,37 @@ class PanmicticInterpolator(_Interpolator):
 
     def __call__(self, t, demo):
         etas = util.coalescent_rates(demo)
-        R = []
-        for p in self.state.pops:
-            R.append(etas[p].R(t) - etas[p].R(self.t0))
-        # no coalescence allowed in the "untracked" deme
-        R.append(0.0)
-        R = jnp.array(R)
-        log_p_prime = jnp.log(self.state.p) - self.C.dot(R)
-        log_s_prime = jax.scipy.special.logsumexp(log_p_prime)
-        p_prime = jnp.exp(log_p_prime - log_s_prime)
-        coal = jnp.array([1 / 2 / etas[p](t) for p in self.state.pops])
-        coal = jnp.append(coal, 0.0)
-        # probability distribution conditional on no coalescence
-        c = jnp.sum(p_prime * self.C.dot(coal))
-        return dict(c=c, log_s=self.state.log_s + log_s_prime, p=p_prime)
+
+        def f(t):
+            R = []
+            for p in self.state.pops:
+                R.append(etas[p].R(t) - etas[p].R(self.t0))
+            # no coalescence allowed in the "untracked" deme
+            R.append(0.0)
+            R = jnp.array(R)
+
+            # prevent nan bugs in backward pass
+            p0 = jnp.isclose(self.state.p, 0.0)
+            psafe = jnp.where(p0, 1.0, self.state.p)
+            log_p_prime = jnp.log(psafe) - self.C.dot(R)
+            log_p_prime = jnp.where(p0, -jnp.inf, log_p_prime)
+
+            log_s_prime = jax.scipy.special.logsumexp(log_p_prime)
+            p_prime = jnp.exp(log_p_prime - log_s_prime)
+            coal = jnp.array([1 / 2 / etas[p](t) for p in self.state.pops])
+            coal = jnp.append(coal, 0.0)
+            # probability distribution conditional on no coalescence
+            c = jnp.sum(p_prime * self.C.dot(coal))
+            return dict(c=c, log_s=self.state.log_s + log_s_prime, p=p_prime)
+
+        tinf = jnp.isinf(t)
+        tsafe = jnp.where(tinf, self.t0, t)
+        ret = f(tsafe)
+        return jax.tree.map(
+            partial(jnp.where, tinf),
+            dict(c=0.0, log_s=self.state.log_s, p=self.state.p),
+            ret,
+        )
 
 
 class ODEInterpolator(_Interpolator):
@@ -55,7 +74,7 @@ class ODEInterpolator(_Interpolator):
         return self.C.dot(eta)
 
     def __call__(self, t, demo):
-        y = self.sol.evaluate(t)
+        y = self.sol.evaluate(jnp.clip(t, self.t0, self.t1))
         p, s = y
         p /= p.sum()
         c = jnp.sum(p * self._rate(t, demo))
@@ -73,9 +92,10 @@ class FilterInterp(eqx.Module):
 
     def __call__(self, t: ScalarLike, demo: dict) -> dict[str, ScalarLike]:
         c = log_s = 0.0
+
         for f in self.interps:
-            y = f(t, demo)
             mask = (f.t0 <= t) & (t < f.t1)
+            y = f(t.clip(f.t0, f.t1), demo)
             c += jnp.where(mask, y["c"], 0.0)
             log_s += jnp.where(mask, y["log_s"], 0.0)
         return dict(c=c, log_s=log_s)
